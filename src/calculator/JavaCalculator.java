10db8b8d7c44546835ebac4b4b18ea9a00e00b96
Ali <83194640+kenpachi01011@users.noreply.github.com>
2021-10-01 21:44:56 +0300

    Update JavaCalculator.java

--- src/calculator/JavaCalculator.java
+++ src/calculator/JavaCalculator.java
@@ -1,17 +1,17 @@
 package calculator;
 
-import java.util.Scanner; //For the Scanner to take input from user.
+import java.util.Scanner; //For the Scanner to take input from user
 
 public class JavaCalculator{
-	public static void main(String[] args) { //the main function.
+	public static void main(String[] args) { //the main function
 		Scanner input = new Scanner(System.in);
 		do {
-			System.out.println("Enter Num1: "); //display this to ask the user for input.
-			int N1= input.nextInt(); //get input.
-			System.out.println("Enter Num2: "); //display this to ask the user for input.
-			int N2= input.nextInt(); // get input.
-			System.out.println("Enter Opretor(0 To Quit): "); //display this to ask the user for input.
-			char OP = input.next().charAt(0); //get input which is an Opreator here for the math calculating.
+			System.out.println("Enter Num1: "); //display this to ask the user for input
+			int N1= input.nextInt(); //get input
+			System.out.println("Enter Num2: "); //display this to ask the user for input
+			int N2= input.nextInt(); // get input
+			System.out.println("Enter Opretor(0 To Quit): "); //display this to ask the user for input
+			char OP = input.next().charAt(0); //get input which is an Opreator here for the math calculating
 			switch(OP) {
 			case'+':
 				System.out.println(N1+N2); //case one if user chossen +
@@ -26,7 +26,7 @@ public class JavaCalculator{
 				System.out.println(N1*N2); //case one if user chossen *
 				break;
 			case'0':
-				System.out.println("Bye, Take care :D"); //print this when the user want to Quit.
+				System.out.println("Bye, Take care :D"); //print this when the user want to Quit
 				break;
 				default:
 					System.out.println("Invalid input, Try agin please ");
