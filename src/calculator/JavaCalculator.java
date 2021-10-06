62957a16e40e83c8810144942ccaac2f936afca2
Ali <83194640+kenpachi01011@users.noreply.github.com>
2021-10-06 23:28:53 +0300

    Update JavaCalculator.java

--- src/calculator/JavaCalculator.java
+++ src/calculator/JavaCalculator.java
@@ -1,13 +1,13 @@
 package calculator;
 
-import java.util.Scanner; //For the Scanner to take input from user.
+import java.util.Scanner; //For the Scanner to take input from user
 
 public class JavaCalculator{
-	public static void main(String[] args) { //the main function.
+	public static void main(String[] args) { //the main function
 		Scanner input = new Scanner(System.in);
 		do {
-			System.out.println("Enter Num1: "); //display this to ask the user for input.
-			int N1= input.nextInt(); //get input.
+			System.out.println("Enter Num1: "); //display this to ask the user for input
+			int N1= input.nextInt(); //get input
 			System.out.println("Enter Num2: "); //display this to ask the user for input.
 			int N2= input.nextInt(); // get input.
 			System.out.println("Enter Opretor(0 To Quit): "); //display this to ask the user for input.
